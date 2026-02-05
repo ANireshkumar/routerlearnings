b916e9b7650919cfab560ac2e8b2e25d5c57f5ae
ANireshkumar <niresh1606@gmail.com>
2026-02-05 05:44:29 +0530

    dev:moved to layoutroute

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
-import Navbar from "./Components/Navbar"
-import {Routes, Route} from 'react-router-dom'
+
+import {Routes, Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom'
 import Home from "./Pages/Home"
 import Contact from "./Pages/Contact"
 import Products from "./Pages/Products"
 import Login from "./Pages/Login"
 import About from "./Pages/About"
+import RootLayout from "./Layout/RootLayout"
 
 
 const App = () => {
+
+const router = createBrowserRouter(createRoutesFromElements((
+  <Route path="/" element={<RootLayout />}>
+    <Route index element={<Home />} />
+    <Route path="Contact" element={<Contact />} />
+    <Route path="products" element={<Products />} />
+    <Route path="about" element={<About />} />  
+  </Route>
+  )));
+
+
   return (
-    <>
-    <div>
-      <Navbar />
-      <Routes>
-       <Route path="/" element={<Home />} />
-       <Route path="/Contact" element={<Contact />} />
-       <Route path="/products" element={<Products />} />
-       <Route path="/about" element={<About />} />
-       <Route path="/login" element={<Login />} />
-      </Routes>
-    </div>
-    
-    </>
+    <RouterProvider router={router} />
   )
 }
 
---
+++ src/Layout/RootLayout.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom"
+import Navbar from "../Components/Navbar"
+
+
+const RootLayout = () => {
+  return (
+
+  <>
+    <Navbar />
+    <Outlet/>
+  </>
+  )
+}
+
+export default RootLayout
